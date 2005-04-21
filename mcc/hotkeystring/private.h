d05bbcc701f6d6a69a3b5fdeb13469ffefa9e302
Allan Odgaard <git@abetarda.com>
2005-04-21 20:52:04 +0000

    - initial LGPL release published at sf.net

---
+++ mcc/hotkeystring/private.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+
+ BetterString.mcc - A better String gadget MUI Custom Class
+ Copyright (C) 1997-2000 Allan Odgaard
+ Copyright (C) 2005 by BetterString.mcc Open Source Team
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ BetterString class Support Site:  http://www.sf.net/projects/bstring-mcc/
+
+ $Id$
+
+***************************************************************************/
+
+#ifndef HOTKEYSTRING_MCC_PRIV_H
+#define HOTKEYSTRING_MCC_PRIV_H
+
+#include <proto/muimaster.h>
+
+#include <muiextra.h>
+#include <mcc_common.h>
+#include <mcc_debug.h>
+
+#include "HotkeyString_mcc.h"
+#include "BetterString_mcc.h"
+
+struct InstData
+{
+	ULONG Flags;
+	struct MUI_EventHandlerNode EventNode;
+};
+
+#define FLG_Active    (1<<0)
+#define FLG_Backspace (1<<1)
+#define FLG_Snoop     (1<<2)
+
+// prototypes
+ULONG HandleInput(struct IClass *cl, Object *obj, struct MUIP_HandleEvent *msg);
+
+#endif /* HOTKEYSTRING_MCC_PRIV_H */
