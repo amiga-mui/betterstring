65b6bf23d5c3148e397ed77691a2dd3f4e293fdc
Jens Maus <mail@jens-maus.de>
2005-07-07 06:41:37 +0000

    - mcc/library.c: added missing USEDCLASSESP define so that MUI knowns   which mcp class belongs to the mcc.

--- mcc/library.c
+++ mcc/library.c
@@ -44,6 +44,9 @@
 #define UserLibID     "$VER: BetterString.mcc " LIB_REV_STRING CPU " (" LIB_DATE ") " LIB_COPYRIGHT
 #define MASTERVERSION 19
 
+#define USEDCLASSESP  used_classesP
+static const STRPTR used_classesP[] = { "BetterString.mcp", NULL };
+
 #define ClassInit
 #define ClassExit
 
