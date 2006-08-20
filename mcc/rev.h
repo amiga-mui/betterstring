ae9ed3f029da305b6543185ba965914dec7fab92
Jens Maus <mail@jens-maus.de>
2006-08-20 15:19:45 +0000

    - last 11.8 release preparations

--- mcc/rev.h
+++ mcc/rev.h
@@ -24,7 +24,7 @@
 #define LIB_REVISION   8
 
 #define LIB_REV_STRING "11.8"
-#define LIB_DATE       "18.08.2006"
+#define LIB_DATE       "20.08.2006"
 
 #if defined(__PPC__)
   #if defined(__MORPHOS__)
