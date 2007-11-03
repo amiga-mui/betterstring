357b3e121be267963058e905e0f1e41ec353c8e3
Jens Maus <mail@jens-maus.de>
2007-11-03 16:16:19 +0000

    * mcp/CreatePrefsGroup.c: added a copyright crawling object to the bottom   of the mcp to match the Look&Feel to our other classes.

--- mcp/CreatePrefsGroup.c
+++ mcp/CreatePrefsGroup.c
@@ -75,49 +75,56 @@ Object *CreatePrefsGroup(struct InstData_MCP *data)
 	Object **objs = data->Objects;
 	Object *group;
 	static const char *key01[2];
-    static const char *key02[2];
-    static const char *key03[2];
-    static const char *key04[2];
-    static const char *key05[2];
-    static const char *key06[2];
-    static const char *key07[2];
-    static const char *key08[2];
-    static const char *key09[2];
-    static const char *key10[2];
-    static const char *key11[2];
-    static const char *key12[2];
-    static const char *key13[2];
-    static const char *key14[2];
-    static const char *key15[2];
-    static const char *key16[2];
-    static const char *key17[2];
-    static const char *key18[2];
+  static const char *key02[2];
+  static const char *key03[2];
+  static const char *key04[2];
+  static const char *key05[2];
+  static const char *key06[2];
+  static const char *key07[2];
+  static const char *key08[2];
+  static const char *key09[2];
+  static const char *key10[2];
+  static const char *key11[2];
+  static const char *key12[2];
+  static const char *key13[2];
+  static const char *key14[2];
+  static const char *key15[2];
+  static const char *key16[2];
+  static const char *key17[2];
+  static const char *key18[2];
+
+  static const char infotext[] = "\033bBetterString.mcp " LIB_REV_STRING "\033n (" LIB_DATE ")\n"
+                                 "Copyright (c) 1997-2000 Allan Odgaard\n"
+                                 LIB_COPYRIGHT "\n\n"
+                                 "Distributed under the terms of the LGPL2.\n\n"
+                                 "For the latest version, check out:\n"
+                                 "http://www.sf.net/projects/bstring-mcc/\n\n";
+
 	static const char **keyinfo[] =
 	{
 	  key01, key02, key03, key04, key05, key06, key07, key08, key09, key10,
 	  key11, key12, key13, key14, key15, key16, key17, key18,
 	  NULL
-    };
+  };
 
-    key01[0] = GetStr(MSG_Help_Copy_Shortcut);                   key01[1] = GetStr(MSG_Help_Copy);
-    key02[0] = GetStr(MSG_Help_Cut_Shortcut);                    key02[1] = GetStr(MSG_Help_Cut);
-    key03[0] = GetStr(MSG_Help_Paste_Shortcut);                  key03[1] = GetStr(MSG_Help_Paste);
-    key04[0] = GetStr(MSG_Help_Undo_Shortcut);                   key04[1] = GetStr(MSG_Help_Undo);
-    key05[0] = GetStr(MSG_Help_Redo_Shortcut);                   key05[1] = GetStr(MSG_Help_Redo);
-    key06[0] = GetStr(MSG_Help_ToggleBuffer_Shortcut);           key06[1] = GetStr(MSG_Help_ToggleBuffer);
-    key07[0] = GetStr(MSG_Help_ToggleCaseChar_Shortcut);         key07[1] = GetStr(MSG_Help_ToggleCaseChar);
-    key08[0] = GetStr(MSG_Help_ToggleCaseWord_Shortcut);         key08[1] = GetStr(MSG_Help_ToggleCaseWord);
-    key09[0] = GetStr(MSG_Help_IncreaseNumber_Shortcut);         key09[1] = GetStr(MSG_Help_IncreaseNumber);
-    key10[0] = GetStr(MSG_Help_DecreaseNumber_Shortcut);         key10[1] = GetStr(MSG_Help_DecreaseNumber);
-    key11[0] = GetStr(MSG_Help_HexToDecimal_Shortcut);           key11[1] = GetStr(MSG_Help_HexToDecimal);
-    key12[0] = GetStr(MSG_Help_DecimalToHex_Shortcut);           key12[1] = GetStr(MSG_Help_DecimalToHex);
-    key13[0] = GetStr(MSG_Help_FilenameCompletition_Shortcut);   key13[1] = GetStr(MSG_Help_FilenameCompletition);
-    key14[0] = GetStr(MSG_Help_Mark_Shortcut);                   key14[1] = GetStr(MSG_Help_Mark);
-    key15[0] = GetStr(MSG_Help_GotoToStartEndOfLine_Shortcut);   key15[1] = GetStr(MSG_Help_GotoToStartEndOfLine);
-    key16[0] = GetStr(MSG_Help_DeleteToStartEndOfLine_Shortcut); key16[1] = GetStr(MSG_Help_DeleteToStartEndOfLine);
-    key17[0] = GetStr(MSG_Help_GotoToPrevNextWord_Shortcut);     key17[1] = GetStr(MSG_Help_GotoToPrevNextWord);
-    key18[0] = GetStr(MSG_Help_DeleteToPrevNextWord_Shortcut);   key18[1] = GetStr(MSG_Help_DeleteToPrevNextWord);
-
+  key01[0] = GetStr(MSG_Help_Copy_Shortcut);                   key01[1] = GetStr(MSG_Help_Copy);
+  key02[0] = GetStr(MSG_Help_Cut_Shortcut);                    key02[1] = GetStr(MSG_Help_Cut);
+  key03[0] = GetStr(MSG_Help_Paste_Shortcut);                  key03[1] = GetStr(MSG_Help_Paste);
+  key04[0] = GetStr(MSG_Help_Undo_Shortcut);                   key04[1] = GetStr(MSG_Help_Undo);
+  key05[0] = GetStr(MSG_Help_Redo_Shortcut);                   key05[1] = GetStr(MSG_Help_Redo);
+  key06[0] = GetStr(MSG_Help_ToggleBuffer_Shortcut);           key06[1] = GetStr(MSG_Help_ToggleBuffer);
+  key07[0] = GetStr(MSG_Help_ToggleCaseChar_Shortcut);         key07[1] = GetStr(MSG_Help_ToggleCaseChar);
+  key08[0] = GetStr(MSG_Help_ToggleCaseWord_Shortcut);         key08[1] = GetStr(MSG_Help_ToggleCaseWord);
+  key09[0] = GetStr(MSG_Help_IncreaseNumber_Shortcut);         key09[1] = GetStr(MSG_Help_IncreaseNumber);
+  key10[0] = GetStr(MSG_Help_DecreaseNumber_Shortcut);         key10[1] = GetStr(MSG_Help_DecreaseNumber);
+  key11[0] = GetStr(MSG_Help_HexToDecimal_Shortcut);           key11[1] = GetStr(MSG_Help_HexToDecimal);
+  key12[0] = GetStr(MSG_Help_DecimalToHex_Shortcut);           key12[1] = GetStr(MSG_Help_DecimalToHex);
+  key13[0] = GetStr(MSG_Help_FilenameCompletition_Shortcut);   key13[1] = GetStr(MSG_Help_FilenameCompletition);
+  key14[0] = GetStr(MSG_Help_Mark_Shortcut);                   key14[1] = GetStr(MSG_Help_Mark);
+  key15[0] = GetStr(MSG_Help_GotoToStartEndOfLine_Shortcut);   key15[1] = GetStr(MSG_Help_GotoToStartEndOfLine);
+  key16[0] = GetStr(MSG_Help_DeleteToStartEndOfLine_Shortcut); key16[1] = GetStr(MSG_Help_DeleteToStartEndOfLine);
+  key17[0] = GetStr(MSG_Help_GotoToPrevNextWord_Shortcut);     key17[1] = GetStr(MSG_Help_GotoToPrevNextWord);
+  key18[0] = GetStr(MSG_Help_DeleteToPrevNextWord_Shortcut);   key18[1] = GetStr(MSG_Help_DeleteToPrevNextWord);
 
 	group = VGroup,
 
@@ -232,21 +239,23 @@ Object *CreatePrefsGroup(struct InstData_MCP *data)
 			MUIA_VertWeight, 10,
 			End,
 
-		Child, CrawlingObject, TextFrame,
-			MUIA_Background,			"m1",
-			MUIA_Virtgroup_Input,	FALSE,
-			MUIA_FixHeightTxt,		"\n\n",
-			Child, TextObject,
-				MUIA_Text_Contents, "BetterString.mcp V" LIB_REV_STRING " (" LIB_DATE ")\n"
-                            "Copyright 1997-2000 Allan Odgaard\n"
-                            LIB_COPYRIGHT "\n\n"
-                            "For the latest version, check out:\n"
-                            "http://www.sf.net/projects/bstring-mcc/\n\n\n\n",
-        MUIA_Text_PreParse, "\33c",
-				End,
-			End,
+    Child, CrawlingObject,
+      TextFrame,
+      MUIA_FixHeightTxt, "\n\n",
+      MUIA_Background,   "m1",
 
-		End;
+      Child, TextObject,
+        MUIA_Text_PreParse, "\033c",
+        MUIA_Text_Contents, infotext,
+      End,
+
+      Child, TextObject,
+        MUIA_Text_PreParse, "\033c",
+        MUIA_Text_Contents, infotext,
+      End,
+    End,
+
+  End;
 
 	if(MUIMasterBase->lib_Version <= 19 && objs[Frame])
 		set(objs[Frame], MUIA_Disabled, TRUE);
