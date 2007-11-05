b0da80b8808951acb8189f74e477bc03107e9dd1
Thore Böckelmann <tboeckel@gmx.de>
2007-11-05 07:29:41 +0000

    * mcc/private.h, mcc/hotkeystring/private.h: added missing rawkey definitions   for OS3.

--- mcc/private.h
+++ mcc/private.h
@@ -96,6 +96,9 @@ struct InstData
   ULONG   MarkedColor;
   ULONG   MarkedTextColor;
   struct  TextFont *Font;
+
+  /* Edit hook */
+  struct Hook *EditHook;
 };
 
 #define FLG_Secret        (1L << 0)
@@ -113,6 +116,74 @@ struct InstData
 #define FLG_NoInput       (1L << 12)
 #define FLG_DragOutside   (1L << 13)
 #define FLG_NoShortcuts   (1L << 14)
+
+// proper RAWKEY_ defines were first introduced in OS4 and MorphOS
+// and unfortunately they are also a bit different, so lets
+// prepare an alternate table for it
+#if defined(__amigaos4__)
+#include <proto/keymap.h>
+
+#define RAWKEY_SCRLOCK    RAWKEY_MENU
+#define RAWKEY_NUMLOCK    0x79
+
+#elif defined(__MORPHOS__)
+#include <devices/rawkeycodes.h>
+
+#define RAWKEY_CRSRUP     RAWKEY_UP
+#define RAWKEY_CRSRDOWN   RAWKEY_DOWN
+#define RAWKEY_CRSRRIGHT  RAWKEY_RIGHT
+#define RAWKEY_CRSRLEFT   RAWKEY_LEFT
+#define RAWKEY_PRINTSCR   RAWKEY_PRTSCREEN
+#define RAWKEY_BREAK      RAWKEY_PAUSE
+
+#define RAWKEY_AUD_STOP       RAWKEY_CDTV_STOP
+#define RAWKEY_AUD_PLAY_PAUSE RAWKEY_CDTV_PLAY
+#define RAWKEY_AUD_PREV_TRACK RAWKEY_CDTV_PREV
+#define RAWKEY_AUD_NEXT_TRACK RAWKEY_CDTV_NEXT
+#define RAWKEY_AUD_SHUFFLE    RAWKEY_CDTV_REW
+#define RAWKEY_AUD_REPEAT     RAWKEY_CDTV_FF
+
+#else
+
+#define RAWKEY_BACKSPACE 0x41
+#define RAWKEY_TAB       0x42
+#define RAWKEY_DEL       0x46
+#define RAWKEY_INSERT    0x47 /* Not on classic keyboards */
+#define RAWKEY_PAGEUP    0x48 /* Not on classic keyboards */
+#define RAWKEY_PAGEDOWN  0x49 /* Not on classic keyboards */
+#define RAWKEY_F11       0x4B /* Not on classic keyboards */
+#define RAWKEY_CRSRUP    0x4C
+#define RAWKEY_CRSRDOWN  0x4D
+#define RAWKEY_CRSRRIGHT 0x4E
+#define RAWKEY_CRSRLEFT  0x4F
+#define RAWKEY_F1        0x50
+#define RAWKEY_F2        0x51
+#define RAWKEY_F3        0x52
+#define RAWKEY_F4        0x53
+#define RAWKEY_F5        0x54
+#define RAWKEY_F6        0x55
+#define RAWKEY_F7        0x56
+#define RAWKEY_F8        0x57
+#define RAWKEY_F9        0x58
+#define RAWKEY_F10       0x59
+#define RAWKEY_HELP      0x5F
+#define RAWKEY_SCRLOCK   0x6B /* Not on classic keyboards */
+#define RAWKEY_PRINTSCR  0x6D /* Not on classic keyboards */
+#define RAWKEY_BREAK     0x6E /* Not on classic keyboards */
+#define RAWKEY_F12       0x6F /* Not on classic keyboards */
+#define RAWKEY_HOME      0x70 /* Not on classic keyboards */
+#define RAWKEY_END       0x71 /* Not on classic keyboards */
+
+#define RAWKEY_AUD_STOP       0x72
+#define RAWKEY_AUD_PLAY_PAUSE 0x73
+#define RAWKEY_AUD_PREV_TRACK 0x74
+#define RAWKEY_AUD_NEXT_TRACK 0x75
+#define RAWKEY_AUD_SHUFFLE    0x76
+#define RAWKEY_AUD_REPEAT     0x77
+
+#define RAWKEY_NUMLOCK   0x79
+
+#endif
 
 // our prototypes
 VOID PrintString(struct IClass *, Object *);
