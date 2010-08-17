2c56b19fc50214e70fbfcdd222e86e92be12d1f4
Thore Böckelmann <tboeckel@gmx.de>
2010-08-17 09:46:54 +0000

    * mccinit.c, SDI_compiler.h: updated to latest versions.

--- mcc/library.c
+++ mcc/library.c
@@ -45,11 +45,9 @@
 #define USERLIBID     CLASS " " LIB_REV_STRING " [" SYSTEMSHORT "/" CPU "] (" LIB_DATE ") " LIB_COPYRIGHT
 #define MASTERVERSION 19
 
-#define USEDCLASSESP  used_classesP
-static const char *used_classesP[] = { "BetterString.mcp", NULL };
-
 #define CLASSINIT
 #define CLASSEXPUNGE
+
 #define MIN_STACKSIZE 8192
 
 struct Library *DiskfontBase = NULL;
