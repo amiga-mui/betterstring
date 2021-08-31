49a34ba39cb3900d8459021384ce3afc4af02b64
Thore Böckelmann <tboeckel@gmx.de>
2021-08-31 20:05:05 +0200

    * Release 11.36 preparations.

--- mcp/version.h
+++ mcp/version.h
@@ -29,8 +29,8 @@
 
 // for setting all necessary version information
 #define LIB_VERSION    11
-#define LIB_REVISION   35
-#define LIB_DATE       "18.12.2020"
+#define LIB_REVISION   36
+#define LIB_DATE       "31.08.2021"
 #define LIB_COPYRIGHT  "Copyright (C) 2005-2021 BetterString.mcc Open Source Team"
 
 // set the LIB_REV_STRING
